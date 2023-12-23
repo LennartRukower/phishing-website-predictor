3d46249c9ad1077ec4a51d163bab7bb54b902a85
LennartRukower <mail@lennart-rukower.de>
2023-12-23 11:33:08 +0100

    feat(models): Refined provider

--- models/FFNetProvider.py
+++ models/FFNetProvider.py
@@ -1,22 +1,32 @@
 from exp.FFNet import FFNet
+import torch
 
 class FFNetProvider():
+
+    model = None
     def __init__(self, model_path, model_features_size):
         self.model_path = model_path
         self.model_features_size = model_features_size
         self.model = None
 
     def load(self):
+        if (self.model is not None):
+            print("Model already loaded")
+            return
         config = {
-                "input": len(self.model_features_size),
-                "output": 2,
-                "hidden": [len(self.model_features_size),  48],
-                "activations": ["ReLU", "Sigmoid"]
-            }
+            "input": self.model_features_size,
+            "output": 1,
+            "hidden": [64, 80, 126, 80, 64, 32],
+            "activations": ["ReLU", "ReLU", "ReLU", "ReLU","ReLU", "ReLU", "Sigmoid"],
+            "dropout_positions": [],
+            "dropout_probs": [],
+        }
         
-        self.model = FFNet(config['input'], config['hidden'], config['output'], config["activations"])
+        self.model = FFNet(config['input'], config['hidden'], config['output'], config["activations"], config["dropout_positions"], config["dropout_probs"])
         self.model.load(self.model_path)
     
     def predict(self, features):
-        return self.model.predict(features)
+        output = self.model.predict(features)
+        pred = torch.round(output.squeeze(1))
+        return pred
 
